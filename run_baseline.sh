c40722971e65798ae1a70a9fede7be0eb7809e16
Louis Lim Ying Wei <gamemaniaz@hotmail.com>
2024-10-30 21:09:51 +0800

    update slurm config

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --time=5-00:00:00
-#SBATCH --gpus=a100:1
+#SBATCH --gpus=a100:2
 #SBATCH --job-name=run_baseline
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=e1374073@u.nus.edu
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 
 python baseline2.py
