1ef8ff65ea52b79c9146a7011159577483322929
Louis Lim Ying Wei <gamemaniaz@hotmail.com>
2024-11-02 15:39:17 +0800

    gen knowledge prompt

---
+++ run_gen_knowledge_prompting.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --time=5-00:00:00
+#SBATCH --gpus=a100-80:1
+#SBATCH --job-name=run_baseline
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=e1374073@u.nus.edu
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=64G
+CONDA_ENV=eedi2
+$HOME/miniforge3/envs/$CONDA_ENV/bin/python gen_knowledge_prompting.py
