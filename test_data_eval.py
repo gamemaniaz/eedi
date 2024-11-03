9b79de5f4fd01301ff8afe26eb8dcdb286c98a6c
Wang Ruoqing <55228709+RuoqingEvelineWang@users.noreply.github.com>
2024-11-03 19:46:17 +0800

    Update test_data_eval.py

--- test_data_eval.py
+++ test_data_eval.py
@@ -1,3 +1,30 @@
+#from baseline.py
+def apk(actual, predicted, k=25):
+    if not actual:
+        return 0.0
+
+    actual = [actual]
+    #comment below line if predicted is already a list
+    predicted = list(map(int, predicted.split()))
+
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
+
+    return score / min(len(actual), k)
+
+#from baseline.py
+def mapk(actual, predicted, k=25):
+    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
+
+
 def process_pred(df):
     df[['QuestionId', 'Answer']] = df['QuestionId_Answer'].str.split('_', expand=True)
 
